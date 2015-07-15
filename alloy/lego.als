830613d62ab15d08ea8ddce306d1860eca42bb27
Aaron Morais <aaron.m@live.ca>
2015-07-14 23:06:31 -0400

    Begin using alloy programatically

---
+++ alloy/lego.als
@@ -0,0 +1,128 @@
+abstract sig Brick {
+	voxels : set Voxel,
+	colour : one Colour
+}
+-- a set comprehension to tell us which bricks are connected to which others
+fun connected[] : Brick->Brick {
+	{b1, b2 : Brick | connected[b1,b2] && b1 != b2 }
+}
+-- the definition of what it means for two bricks to be connected
+-- functions and predicates are in different name spaces,
+-- so it is ok to have a pred and a fun with the same name
+pred connected[b1, b2 : one Brick] {
+	some v1 : b1.voxels |
+			some v2 : b2.voxels | aboveOrBelow[v1, v2]
+}
+sig OneOne, OneTwo, OneThree, OneFour, TwoTwo, LBlock, TwoThree, TwoFour,
+	OneOnePlate, OneTwoPlate, OneThreePlate, OneFourPlate, LPlate,
+	TwoTwoPlate, TwoThreePlate, TwoFourPlate, ThreeThreePlate extends Brick {}
+fun length[] : Brick -> Int {
+	OneOne->1 + OneTwo->2 + OneThree->3 + OneFour->4 +
+		TwoTwo->2 + LBlock->2 + TwoThree->2 + TwoFour->2 +
+		OneOnePlate->1 + OneTwoPlate->2 + OneThreePlate->3 + OneFourPlate->4 +
+		LPlate->2 + TwoTwoPlate->2 + TwoThreePlate->2 + TwoFourPlate->2 +
+		ThreeThreePlate -> 3}
+fun width[] : Brick -> Int {
+	OneOne->1 + OneTwo->1 + OneThree->1 + OneFour->1 +
+		TwoTwo->2 + LBlock->2 + TwoThree->3 + TwoFour->4 +
+		OneOnePlate->1 + OneTwoPlate->1 + OneThreePlate->1 + OneFourPlate->1 +
+		LPlate->2 + TwoTwoPlate->2 + TwoThreePlate->3 + TwoFourPlate->4 +
+		ThreeThreePlate -> 3}
+fun height[] : Brick -> Int {
+	OneOne->3 + OneTwo->3 + OneThree->3 + OneFour->3 +
+		TwoTwo->3 + LBlock->3 + TwoThree->3 + TwoFour->3 +
+		OneOnePlate->1 + OneTwoPlate->1 + OneThreePlate->1 + OneFourPlate->1 +
+		LPlate->1 + TwoTwoPlate->1 + TwoThreePlate->1 + TwoFourPlate->1 +
+		ThreeThreePlate -> 1}
+fun volume[] : Brick -> Int {
+	OneOne->3 + OneTwo->6 + OneThree->9 + OneFour->12 +
+		TwoTwo->12 + LBlock->9 + TwoThree->18 + TwoFour->24 +
+		OneOnePlate->1 + OneTwoPlate->2 + OneThreePlate->3 + OneFourPlate->4 +
+		LPlate->3 + TwoTwoPlate->4 + TwoThreePlate->6 + TwoFourPlate->8 +
+		ThreeThreePlate -> 9}
+fun inventory[] : Brick -> Int {
+	OneOne->1 + OneTwo->1 + OneThree->1 + OneFour->1 +
+		TwoTwo->1 + LBlock->1 + TwoThree->1 + TwoFour->1 +
+		OneOnePlate->5 + OneTwoPlate->4 + OneThreePlate->0 + OneFourPlate->4 +
+		LPlate->0 + TwoTwoPlate->0 + TwoThreePlate->1 + TwoFourPlate->8 +
+		ThreeThreePlate -> 1}
+
+abstract sig Colour {}
+one sig Red, Blue, Yellow extends Colour {}
+
+pred above[v1, v2 : one Voxel] {
+	v1.x = v2.x
+	v1.z = v2.z
+	v1.y = next[v2.y]
+}
+pred aboveOrBelow[v1, v2 : one Voxel] {
+	above[v1,v2] or above[v2,v1]
+}
+pred xAdjacent[v1,v2 : one Voxel] {
+	v1.y = v2.y
+	v1.z = v2.z
+	v1.x = next[v2.x] or next[v1.x] = v2.x
+}
+pred zAdjacent[v1,v2 : one Voxel] {
+	v1.y = v2.y
+	v1.x = v2.x
+	v1.z = next[v2.z] or next[v1.z] = v2.z
+}
+fun adjacent[] : Voxel -> Voxel {
+	{v1, v2 : Voxel | xAdjacent[v1,v2] or zAdjacent[v1,v2] or aboveOrBelow[v1,v2]}
+}
+fun xAdjacent[] : Voxel -> Voxel {
+	{v1, v2 : Voxel | xAdjacent[v1,v2]}
+}
+fun zAdjacent[] : Voxel -> Voxel {
+	{v1, v2 : Voxel | zAdjacent[v1,v2] }
+}
+fun yAdjacent[] : Voxel -> Voxel {
+	{v1, v2 : Voxel | aboveOrBelow[v1,v2] }
+}
+
+fact WellPlaced {
+	-- all Voxels are assigned to some brick
+	Brick.voxels = Voxel
+	-- Bricks have the proper volume
+	all b : Brick | b.volume = #(b.voxels)
+	-- Each Voxel is occupied by exactly one Brick
+	all v : Voxel | one voxels.v
+	-- Each brick occupies contiguous voxels
+	all b : Brick | all v: b.voxels | b.voxels = v.*(b.voxels <: adjacent :> b.voxels)
+	-- Every brick is connected to some other brick
+	(#Brick > 1) => Brick in Brick.connected
+	-- Every brick is (transitively) connected to every other brick
+	all b : Brick | b.*connected = Brick
+	-- Bricks are only as long and wide as the brick type allows
+	all b : Brick | all v : b.voxels |
+		 (#(v.*(b.voxels <: xAdjacent :> b.voxels)) <= b.length and
+			 #(v.*(b.voxels <: zAdjacent :> b.voxels)) <= b.width) or
+ 		(#(v.*(b.voxels <: xAdjacent :> b.voxels)) <= b.width and
+			 #(v.*(b.voxels <: zAdjacent :> b.voxels)) <= b.length)
+ 	-- Each voxel in the brick must be the same colour as the brick
+	all b : Brick | all v : b.voxels | v.colour = b.colour
+	--	Each brick is only as tall as brick allows
+	all b : Brick | all v : b.voxels |
+		 #(v.*(b.voxels <: yAdjacent :> b.voxels)) <= b.height
+	-- There must be at least one TwoTwo brick used
+	#(OneOne <: Brick) <= OneOne.inventory
+	#(OneTwo <: Brick) <= OneTwo.inventory
+	#(OneThree <: Brick) <= OneThree.inventory
+	#(OneFour <: Brick) <= OneFour.inventory
+	#(TwoTwo <: Brick) <= TwoTwo.inventory
+	#(TwoThree <: Brick) <= TwoThree.inventory
+	#(LBlock <: Brick) <= LBlock.inventory
+	#(TwoFour <: Brick) <= TwoFour.inventory
+	#(OneTwoPlate <: Brick) <= OneTwoPlate.inventory
+	#(OneOnePlate <: Brick) <= OneOnePlate.inventory
+	#(OneThreePlate <: Brick) <= OneThreePlate.inventory
+	#(OneFourPlate <: Brick) <= OneFourPlate.inventory
+	#(TwoTwoPlate <: Brick) <= TwoTwoPlate.inventory
+	#(TwoThreePlate <: Brick) <= TwoThreePlate.inventory
+	#(TwoFourPlate <: Brick) <= TwoFourPlate.inventory
+	#(LPlate <: Brick) <= LPlate.inventory
+	#(ThreeThreePlate <: Brick) <= ThreeThreePlate.inventory
+}
+pred build {}
+run build for 8 but 8 Int
---
+++ alloy/src/Lego.java
@@ -0,0 +1,153 @@
+import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.StringWriter;
+import java.util.ArrayList;
+import java.util.Iterator;
+import org.json.simple.JSONArray;
+import java.util.LinkedHashMap;
+import java.util.LinkedList;
+import java.util.Map;
+import org.json.simple.JSONValue;
+import java.io.PrintWriter;
+
+import edu.mit.csail.sdg.alloy4.A4Reporter;
+import edu.mit.csail.sdg.alloy4.XMLNode;
+import edu.mit.csail.sdg.alloy4compiler.ast.Command;
+import edu.mit.csail.sdg.alloy4compiler.ast.Expr;
+import edu.mit.csail.sdg.alloy4compiler.ast.Module;
+import edu.mit.csail.sdg.alloy4compiler.parser.CompUtil;
+import edu.mit.csail.sdg.alloy4compiler.translator.A4Options;
+import edu.mit.csail.sdg.alloy4compiler.translator.A4Solution;
+import edu.mit.csail.sdg.alloy4compiler.translator.A4SolutionReader;
+import edu.mit.csail.sdg.alloy4compiler.translator.A4Tuple;
+import edu.mit.csail.sdg.alloy4compiler.translator.A4TupleSet;
+import edu.mit.csail.sdg.alloy4compiler.translator.TranslateAlloyToKodkod;
+
+public class Lego {
+  private static String lego_model_file_name = "lego.als";
+  private static String output_xml_file_name = "lego.xml";
+  private static String output_json_file_name = "lego.json";
+
+  public static void main(String[] args) throws Exception {
+    String modelName = getModelName(args);
+    if (modelName == null) {
+      System.err.println("No model name provided");
+    }
+
+    String lego_model = readFile(modelName);
+    lego_model += "\n" + readFile(lego_model_file_name);
+
+    // TODO: Fully understand the black box below.
+    A4Reporter rep = new A4Reporter();
+    File tmpAls = File.createTempFile("alloyEvaluator", ".als");
+    tmpAls.deleteOnExit();
+    flushModelToFile(lego_model, tmpAls);
+    Module world = CompUtil.parseEverything_fromFile(rep, null, tmpAls.getAbsolutePath());
+    A4Options opt = new A4Options();
+    opt.originalFilename = tmpAls.getAbsolutePath();
+    opt.solver = A4Options.SatSolver.SAT4J;
+    Command cmd = world.getAllCommands().get(0);
+    A4Solution sol = TranslateAlloyToKodkod.execute_commandFromBook(rep, world.getAllReachableSigs(), cmd, opt);
+    assert sol.satisfiable();
+    sol.writeXML(output_xml_file_name);
+
+    Map voxels = new LinkedHashMap();
+    Expr xVoxel = CompUtil.parseOneExpression_fromString(world, "Voxel<:x");
+    Iterator<A4Tuple> itr = ((A4TupleSet)sol.eval(xVoxel)).iterator();
+    while (itr.hasNext()) {
+      A4Tuple element = (A4Tuple)itr.next();
+      voxels.put(element.atom(0), new LinkedHashMap());
+    }
+    itr = ((A4TupleSet)sol.eval(xVoxel)).iterator();
+    while (itr.hasNext()) {
+      A4Tuple element = (A4Tuple)itr.next();
+      ((LinkedHashMap)voxels.get(element.atom(0))).put("x", element.atom(1));
+    }
+
+    Expr yVoxel = CompUtil.parseOneExpression_fromString(world, "Voxel<:y");
+    itr = ((A4TupleSet)sol.eval(yVoxel)).iterator();
+    while (itr.hasNext()) {
+      A4Tuple element = (A4Tuple)itr.next();
+      ((LinkedHashMap)voxels.get(element.atom(0))).put("y", element.atom(1));
+    }
+
+    Expr zVoxel = CompUtil.parseOneExpression_fromString(world, "Voxel<:z");
+    itr = ((A4TupleSet)sol.eval(xVoxel)).iterator();
+    while (itr.hasNext()) {
+      A4Tuple element = (A4Tuple)itr.next();
+      ((LinkedHashMap)voxels.get(element.atom(0))).put("z", element.atom(1));
+    }
+
+    Map voxelToBrick = new LinkedHashMap();
+    Expr voxelToBrickExpr = CompUtil.parseOneExpression_fromString(world, "Brick<:voxels");
+    itr = ((A4TupleSet)sol.eval(voxelToBrickExpr)).iterator();
+    while (itr.hasNext()) {
+      A4Tuple element = (A4Tuple)itr.next();
+      voxelToBrick.put(element.atom(1), element.atom(0));
+    }
+
+    LinkedList brickList = new LinkedList();
+    Expr brickExpr = CompUtil.parseOneExpression_fromString(world, "Brick");
+    itr = ((A4TupleSet)sol.eval(brickExpr)).iterator();
+    while (itr.hasNext()) {
+      A4Tuple element = (A4Tuple)itr.next();
+      brickList.add(element.atom(0));
+    }
+
+    Map list = new LinkedHashMap();
+    list.put("bricks", brickList);
+    list.put("voxelToBrick", voxelToBrick);
+    list.put("voxels", voxels);
+    StringWriter listOut = new StringWriter();
+    JSONValue.writeJSONString(list, listOut);
+    String listJSON = listOut.toString();
+
+    PrintWriter out = new PrintWriter(output_json_file_name);
+    out.println(listJSON);
+    out.close();
+  }
+
+  private static void flushModelToFile(String model, File tmpAls) throws IOException {
+    BufferedOutputStream bos = null;
+    try {
+      bos = new BufferedOutputStream(new FileOutputStream(tmpAls));
+      bos.write(model.getBytes());
+      bos.flush();
+    } finally {
+      if (bos != null)
+          bos.close();
+    }
+  }
+
+  private static String getModelName(String[] args) {
+    if (args[0].equals("-model")) {
+      if (1 < args.length) {
+        return args[1];
+      } else {
+        System.err.println("-model requires a model name");
+      }
+    }
+    return null;
+  }
+
+  private static String readFile(String fileName) throws IOException {
+    BufferedReader br = new BufferedReader(new FileReader(fileName));
+    try {
+        StringBuilder sb = new StringBuilder();
+        String line = br.readLine();
+
+        while (line != null) {
+            sb.append(line);
+            sb.append("\n");
+            line = br.readLine();
+        }
+        return sb.toString();
+    } finally {
+        br.close();
+    }
+  }
+}
